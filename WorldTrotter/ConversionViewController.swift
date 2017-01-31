b31aee2f5888d06185870d72b0d714baba0d1f27
Alexander Nardozzi <anardozzi@half-life.local>
2017-01-30 21:10:53 -0500

    can calculated C from F

---
+++ WorldTrotter/ConversionViewController.swift
@@ -0,0 +1,53 @@
+//
+//  ConversionViewController.swift
+//  WorldTrotter
+//
+//  Created by Alexander Nardozzi on 1/30/17.
+//  Copyright © 2017 Alex Nardozzi. All rights reserved.
+//
+
+import UIKit
+
+class ConversionViewController: UIViewController{
+    @IBOutlet var celsiusLabel: UILabel!
+    @IBOutlet var textField: UITextField!
+    var fahrenheitValue: Measurement<UnitTemperature>? {
+        didSet {
+            updateCelsiusLabel()
+        }
+    }
+    
+    var celsiusValue: Measurement<UnitTemperature>? {
+        if let fahrenheitValue = fahrenheitValue {
+            return fahrenheitValue.converted(to: .celsius)
+        } else {
+            return nil
+        }
+    }
+    
+    func updateCelsiusLabel(){
+        if let celsiusValue = celsiusValue {
+            celsiusLabel.text = "\(celsiusValue.value)"
+        } else {
+            celsiusLabel.text = "???"
+        }
+    }
+    
+    @IBAction func fahrenheitFieldEditingChanged(_ textField: UITextField){
+        if let text = textField.text, let value = Double(text) {
+            fahrenheitValue = Measurement(value: value, unit: .fahrenheit)
+        } else {
+            fahrenheitValue = nil
+        }
+    }
+    
+    @IBAction func dismissKeyboard(_ sender: UITapGestureRecognizer) {
+        textField.resignFirstResponder()
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        updateCelsiusLabel()
+    }
+}
