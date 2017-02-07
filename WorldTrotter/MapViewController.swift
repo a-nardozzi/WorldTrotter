bb3c5f1b9978a09aa7923ace61f26c0c0e0f248d
Alexander Nardozzi <anardozzi@half-life.local>
2017-02-07 18:01:25 -0500

    completed chapter 5

---
+++ WorldTrotter/MapViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MapViewController.swift
+//  WorldTrotter
+//
+//  Created by Alexander Nardozzi on 2/7/17.
+//  Copyright © 2017 Alex Nardozzi. All rights reserved.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+    override func viewDidLoad(){
+        super.viewDidLoad()
+        
+        print("MapViewController loaded its view.")
+    }
+}
