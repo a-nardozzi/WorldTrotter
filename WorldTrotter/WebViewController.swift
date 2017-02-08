a3e295a93f47c053ee2b9f573cac8e8a8872de4e
Alexander Nardozzi <anardozzi@half-life.local>
2017-02-07 20:21:26 -0500

    completed chapter 6 bronze challenge

--- WorldTrotter/WebViewController.swift
+++ WorldTrotter/WebViewController.swift
@@ -11,21 +11,18 @@ import UIKit
 import WebKit
 
 
-class WebViewController: UIViewController, WKUIDelegate {
+class WebViewController: UIViewController {
     var webView: WKWebView!
     
     override func loadView() {
-        let webConfiguration = WKWebViewConfiguration()
-        webView = WKWebView(frame: .zero, configuration: webConfiguration)
-        webView.uiDelegate = self
-        
+        webView = WKWebView()
         view = webView
         
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        let myURL = URL(string: "http://www.google.com")
+        let myURL = URL(string: "https://www.bignerdranch.com")
         let myRequest = URLRequest(url: myURL!)
         
         webView.load(myRequest)
