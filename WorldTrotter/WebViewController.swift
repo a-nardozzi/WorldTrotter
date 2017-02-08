9459b3e5025b470556f95c7b4689bc176cd18d1e
Alexander Nardozzi <anardozzi@mystique-2.local>
2017-02-07 19:48:10 -0500

    attempted bronze challenge

---
+++ WorldTrotter/WebViewController.swift
@@ -0,0 +1,35 @@
+//
+//  WebViewController.swift
+//  WorldTrotter
+//
+//  Created by Alexander Nardozzi on 2/7/17.
+//  Copyright © 2017 Alex Nardozzi. All rights reserved.
+//
+
+
+import UIKit
+import WebKit
+
+
+class WebViewController: UIViewController, WKUIDelegate {
+    var webView: WKWebView!
+    
+    override func loadView() {
+        let webConfiguration = WKWebViewConfiguration()
+        webView = WKWebView(frame: .zero, configuration: webConfiguration)
+        webView.uiDelegate = self
+        view = webView
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print("view loaded")
+        
+        let myURL = URL(string: "http://www.bignerdranch.com")
+        let myRequest = URLRequest(url: myURL!)
+        
+        webView.load(myRequest)
+    }
+    
+}
