2881eff2b4537149c1e2d58b9d2cf6ecf965ede4
Alexander Nardozzi <anardozzi@mystique.highpoint.edu>
2017-02-13 13:46:28 -0500

    add find me functionality, started pins

--- WorldTrotter/MapViewController.swift
+++ WorldTrotter/MapViewController.swift
@@ -14,33 +14,46 @@ class MapViewController: UIViewController, CLLocationManagerDelegate, MKMapViewD
     
     var mapView: MKMapView!
     
+    var lastLoc: MKCoordinateRegion!
+    
+    var currentPress = true
     
     
     let locationManager = CLLocationManager()
     
-    func buttonAction(sender: UIButton!){
-        mapView.showsUserLocation = true
-   //   print(mapView.userLocation.location)
-        
-        //locationManager = CLLocationManager()
-        
- 
-        
+    func locateMe(sender: UIButton!){
+        if(currentPress){
+            mapView.showsUserLocation = true
+            mapView.showsUserLocation = false
+            currentPress = false
+        } else {
+            mapView.setRegion(lastLoc, animated: true)
+            currentPress = true
+        }
     }
     
     func mapViewWillStartLocatingUser(_ mapView: MKMapView) {
-        print("Start Loading")
+        print("Started Loading Location")
+        
+        
         
         locationManager.delegate = self
         locationManager.desiredAccuracy = kCLLocationAccuracyBest
         locationManager.startUpdatingLocation()
         
+        lastLoc = mapView.region
+        
+        let region = MKCoordinateRegionMakeWithDistance((locationManager.location?.coordinate)!, 1000, 1000)
+        mapView.setRegion(region, animated: true)
+        
+        
     }
     
     func mapViewDidStopLocatingUser(_ mapView: MKMapView) {
-        print("Stop Loading")
+        print("Stopped Loading Location")
     }
     
+  
 
     
     
@@ -49,6 +62,8 @@ class MapViewController: UIViewController, CLLocationManagerDelegate, MKMapViewD
         
         view = mapView
         self.mapView.delegate = self
+        locationManager.requestAlwaysAuthorization()
+        
         let standardString = NSLocalizedString("Standard", comment: "Standard Map View")
         let satelliteString = NSLocalizedString("Satellite", comment: "Satellite Map View")
         let hybridString = NSLocalizedString("Hybrid", comment: "Hybrid Map View")
@@ -72,6 +87,7 @@ class MapViewController: UIViewController, CLLocationManagerDelegate, MKMapViewD
         trailingConstraint.isActive = true
         
         
+        // -----User Location Finder
         let userLoc = UIButton()
         userLoc.backgroundColor = UIColor.white.withAlphaComponent(0.5)
         userLoc.layer.borderWidth = 1
@@ -80,7 +96,8 @@ class MapViewController: UIViewController, CLLocationManagerDelegate, MKMapViewD
         userLoc.translatesAutoresizingMaskIntoConstraints = false
         userLoc.setTitle(" Find Me ", for: .normal)
         userLoc.setTitleColor(UIColor.blue, for: .normal)
-        userLoc.addTarget(self, action: #selector(buttonAction), for: .touchUpInside)
+        userLoc.addTarget(self, action: #selector(locateMe), for: .touchUpInside)
+        
         userLoc.tag = 1
         view.addSubview(userLoc)
         
@@ -89,6 +106,51 @@ class MapViewController: UIViewController, CLLocationManagerDelegate, MKMapViewD
         
         userLocBotConstraint.isActive = true
         userLocLeadingConstraint.isActive = true
+        
+        //-----Pin Tracker
+        let bornAnnotation = MKPointAnnotation()
+        bornAnnotation.coordinate = CLLocationCoordinate2D(latitude: 42.2513, longitude: -71.0773)
+        bornAnnotation.title = "Born Here"
+        let currentAnnotation = MKPointAnnotation()
+        currentAnnotation.coordinate = CLLocationCoordinate2D(latitude: 35.9732, longitude: -79.9950)
+        currentAnnotation.title = "Currently Here"
+        let interestAnnotation = MKPointAnnotation()
+        interestAnnotation.coordinate = CLLocationCoordinate2D(latitude: 44.5437, longitude: -72.8143)
+        interestAnnotation.title = "Been Here"
+        
+        mapView.addAnnotation(bornAnnotation)
+        mapView.addAnnotation(currentAnnotation)
+        mapView.addAnnotation(interestAnnotation)
+        
+        let pins = UIButton()
+        pins.backgroundColor = UIColor.white.withAlphaComponent(0.5)
+        pins.layer.borderWidth = 1
+        pins.layer.borderColor = UIColor.blue.cgColor
+        pins.layer.cornerRadius = 5
+        pins.translatesAutoresizingMaskIntoConstraints = false
+        pins.setTitle(" Pins ", for: .normal)
+        pins.setTitleColor(UIColor.blue, for: .normal)
+        
+    
+        
+        pins.tag = 2
+        view.addSubview(pins)
+        
+        let pinsBotConstraint = pins.bottomAnchor.constraint(equalTo: bottomLayoutGuide.topAnchor, constant: -8)
+        let pinsTrailingConstraint = pins.trailingAnchor.constraint(equalTo: margins.trailingAnchor)
+        
+        pinsBotConstraint.isActive = true
+        pinsTrailingConstraint.isActive = true
+        
+    }
+    
+    var pinNumber = 0
+    func cyclePins(){
+        var rgn: CLLocationCoordinate2D
+        if(pinNumber == 0) {
+            rgn.init(latitude: 42.2513, longitude: -71.0773)
+            MKCoordinateRegionMakeWithDistance(rgn, 1000, 1000)
+        }
         
         
     }
